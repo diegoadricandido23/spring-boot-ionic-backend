f9bc8a6adfed2321c9bd4ae0a914a6e670650cb6
Diego Adriano <diegoadricandido23@gmail.com>
2018-06-08 00:16:07 -0300

    Banco de Dados H2 e criacao automatica da base de dados

--- src/main/java/com/diego/cursomc/domain/Categoria.java
+++ src/main/java/com/diego/cursomc/domain/Categoria.java
@@ -5,14 +5,23 @@ package com.diego.cursomc.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * @author Diego Adriano
  * @since 06/07/2018
  */
+@Entity
 public class Categoria implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	
 	public Categoria() {
