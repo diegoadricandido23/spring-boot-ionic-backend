1d300df0894eb3328229aaf04171461afc0feb15
Diego Adriano <diegoadricandido23@gmail.com>
2018-07-16 21:19:00 -0300

    Validacao customizada: email nao repetido na insercao de Cliente

--- src/main/java/com/diego/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/diego/cursomc/repositories/ClienteRepository.java
@@ -5,8 +5,8 @@ package com.diego.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.diego.cursomc.domain.Cidade;
 import com.diego.cursomc.domain.Cliente;
 
 /**
@@ -17,4 +17,6 @@ import com.diego.cursomc.domain.Cliente;
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer>{
 	
+	@Transactional(readOnly=true)
+	Cliente findByEmail(String email);
 }
--- src/main/java/com/diego/cursomc/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/diego/cursomc/services/validation/ClienteInsertValidator.java
@@ -11,8 +11,12 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.diego.cursomc.domain.Cliente;
 import com.diego.cursomc.domain.enums.TipoCliente;
 import com.diego.cursomc.dto.ClienteNewDTO;
+import com.diego.cursomc.repositories.ClienteRepository;
 import com.diego.cursomc.resources.exception.FieldMessage;
 import com.diego.cursomc.services.validation.utils.BR;
 
@@ -22,6 +26,9 @@ import com.diego.cursomc.services.validation.utils.BR;
  */
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
 
+	@Autowired
+	private ClienteRepository repo;
+	
 	@Override
 	public void initialize(ClienteInsert constraintAnnotation) {
 
@@ -38,7 +45,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		if (clienteNewDTO.getTipo().equals(TipoCliente.PESSOAJURICA.getCod())
 				&& !BR.isValidCnpj(clienteNewDTO.getCpfOuCnpj()))
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Invalido"));
-
+		
+		Cliente aux = repo.findByEmail(clienteNewDTO.getEmail());
+		if(aux != null)
+			list.add(new FieldMessage("email", "Email ja Cadastrado"));
+		
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
 			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
